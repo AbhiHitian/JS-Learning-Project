b494710b076b8410e62a43ed3ebc5cb606ff6dff
AbhiHitian <146157132+AbhiHitian@users.noreply.github.com>
2023-09-27 05:27:53 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId=233455  
+let accountEmail="Abhianhbah@gamil"
+var accountPassward="12345"
+accountCity="Spj"
+let accountState; //  if we declare the varible without any value then JS will treat as undefined 
+
+//accountId=2344  //can 't be changed once we declare the value 
+/*
+we can declare variable with let and var. Var having *block scope and functional scope problem so we will use let .
+*/
+
+console.log(accountId);
+accountEmail="abbv"
+accountPassward="28937"
+accountCity="djkh"
+
+console.table([accountId,accountEmail,accountPassward,accountCity])
+
