e9d8506f4efdfb717621bc026d8a8dbcd4f7ad5b
AbhiHitian <146157132+AbhiHitian@users.noreply.github.com>
2023-10-01 09:55:07 +0000

    type conversion

--- 01_basics/03_dataTypeConversion.js
+++ 01_basics/03_dataTypeConversion.js
@@ -14,3 +14,35 @@ let ConversionboolloggedIn=Boolean(isLoggedIn) // it will return as true
 ""=>false
 "Abhi"=> true
 */
+
+
+//**********operations */
+let value=3
+let negValue=-value
+console.log(negValue)
+
+console.log(2**2)//2 ka power 2
+console.log(2**3)// 2 ka power 3
+console.log(2%3)// to find the reminder 
+
+let str1="Hello"
+
+let str2=" Abhi"
+let str3=str1+str2 // no issue output will be Hello Abhi
+
+//Tricky conversions
+console.log("1"+2) //output=12
+console.log("1"+2+2) //output =122
+console.log(1+2+"2")// output=32 
+console.log(true) // output=true 
+console.log(+true) // output=1
+console.log(+"") //output=0
+
+let num1,num2,num3
+num1=num2=num3=2+2//less redability
+
+
+
+
+
+
